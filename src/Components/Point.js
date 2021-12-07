c19df560dbb4245ad61866732be2c4c8714495ed
Ewelina <ekowalczyk1994@gmail.com>
2021-12-07 10:15:52 -0600

    new inputs was added to the form

--- src/Components/Point.js
+++ src/Components/Point.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Marker, Popup } from 'react-leaflet'
 import {Icon} from "leaflet"
-import 'leaflet/dist/leaflet.css'
+
 
 const geoIcon =new Icon({
     iconUrl: "/geoicon.svg",
@@ -12,15 +12,15 @@ function Point ({points}){
     return(
         <>
         {
-            points.map((point)=>{
-                return(
-                    <Marker key={point.id} position={[point.coordinates[1], point.coordinates[0]]} icon={geoIcon}>
-                     <Popup position={[point.coordinates[1], point.coordinates[0]]}>
-                         <div>
-                             <h1>{point.id}</h1>
-                         </div>
-                    </Popup>    
-                    </Marker>
+        points.map((point)=>{
+         return(
+            <Marker key={point.id} position={[point.coordinates[1], point.coordinates[0]]} icon={geoIcon}>
+            <Popup position={[point.coordinates[1], point.coordinates[0]]}>
+                <div>
+                <h3>{"Borehole: " + point.name}</h3>
+                </div>
+                </Popup>    
+                </Marker>
                 )    
             })
         }
--- src/Components/PointsForm.js
+++ src/Components/PointsForm.js
@@ -1,23 +1,61 @@
 
 import React from 'react'
+import '../App.css';
 
 function PointsForm(){
 return(
-    <div>
-    <form>
-        <h3>Type...</h3>
+    <div class ="form-container">
+    <form class="form-inline">
+        <label for="text">Name</label>
         <input
         type="text"
         name="name"
-        placeholder="Type..."
+        placeholder="Type borehole name"
         />
         <br/>
+        <label for="date">Date</label>
         <input
-        type="text"
-        name="name"
-        placeholder="Type..."
+        type="date"
+        name="date"
+        placeholder="Type date"
         />
          <br/>
+         <label for="text">Ground Elevation </label>
+        <input
+        type="text"
+        name="elevation"
+        placeholder="Type elevation"
+        />
+        <br/>
+        <label for="text">Description</label>
+        <input
+        type="text"
+        name="description"
+        placeholder="Type description"
+        />
+        <br/>
+        <label for="text">Properties</label>
+        <input
+        type="text"
+        name="properties"
+        placeholder="Type properties"
+        />
+        <br/>
+        <label for="text">Classification</label>
+        <input
+        type="text"
+        name="classification"
+        placeholder="Type classification"
+        />
+        <br/>
+        <label for="number">Blow Counts</label>
+        <input
+        type="number"
+        name="blowcounts"
+        placeholder="Type blow counts"
+        />
+        <br/>
+
         <input
         type="submit"
         name="submit"
