af9b4fe321460753b33f04bc5fef5b981529a765
Ewelina <ekowalczyk1994@gmail.com>
2021-12-09 14:15:32 -0600

    component Contacts was styled

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -1,12 +1,34 @@
 import React from 'react'
+import styled from 'styled-components';
 
 function Contacts(){
     return(
-        <div>
-        <h1>Hello</h1>
-        </div>
+        <Wrapper>
+            <ContactRow>
+                <ContactColumn>
+                 <h3>Call Us: ( 111-111-1111)</h3>
+                 <h3>Send an Email: contact@contact.net</h3>
+                </ContactColumn>
+            </ContactRow>
+        </Wrapper>
     )
 }
 
 export default Contacts
 
+const Wrapper = styled.div`
+background-color: #2f3e46;
+`
+
+const ContactRow=styled.div`
+display:grid;
+grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+grid-gap: 30px;
+`
+
+const ContactColumn=styled.div`
+padding:30px;
+h3{
+    color:white;
+}
+`
