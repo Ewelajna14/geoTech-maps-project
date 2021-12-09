cbaeeae87f6fcdc3880848586b5c40957398c6bb
Ewelina <ekowalczyk1994@gmail.com>
2021-12-09 17:55:15 -0600

    routing was fixed

--- src/index.js
+++ src/index.js
@@ -10,9 +10,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
     <React.StrictMode>
       <BrowserRouter>
-      <Route exact path="/">
     <App/>
-    </Route>
     </BrowserRouter>
     </React.StrictMode>,
   document.getElementById('root')
